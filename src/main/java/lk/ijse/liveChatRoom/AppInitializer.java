e71a7858502048684784fa7d3455ed583add8476
Gayanuka Bulegoda <grbulegoda@gmail.com>
2024-01-15 03:35:51 +0530

    initial commit

---
+++ src/main/java/lk/ijse/liveChatRoom/AppInitializer.java
@@ -0,0 +1,24 @@
+package lk.ijse.liveChatRoom;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class AppInitializer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(
+                new Scene(
+                        FXMLLoader.load(getClass().getResource("/view/loginForm.fxml"))));
+
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.centerOnScreen();
+        stage.show();
+    }
+}
