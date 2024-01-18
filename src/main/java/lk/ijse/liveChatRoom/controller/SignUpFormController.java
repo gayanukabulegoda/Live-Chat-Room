cb3ac7b633f9f7daf49cd4a30302e286349a2276
Gayanuka Bulegoda <grbulegoda@gmail.com>
2024-01-18 20:51:32 +0530

    SignUp UI set Designed

---
+++ src/main/java/lk/ijse/liveChatRoom/controller/SignUpFormController.java
@@ -0,0 +1,46 @@
+package lk.ijse.liveChatRoom.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class SignUpFormController {
+
+    @FXML
+    private Label lblUserNameAlert;
+
+    @FXML
+    private Label lblUserNameConfirmAlert;
+
+    @FXML
+    private TextField txtConfirmUserName;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnBackOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signInBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signUpBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtConfirmUserNameOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtUserNameOnAction(ActionEvent event) {
+
+    }
+}
