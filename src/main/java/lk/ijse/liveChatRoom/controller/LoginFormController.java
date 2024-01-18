e0856461b14cf513e3a540da21d4ca23b94efc0a
Gayanuka Bulegoda <grbulegoda@gmail.com>
2024-01-18 17:05:40 +0530

    SignIn UI Designed

--- src/main/java/lk/ijse/liveChatRoom/controller/LoginFormController.java
+++ src/main/java/lk/ijse/liveChatRoom/controller/LoginFormController.java
@@ -1,4 +1,31 @@
 package lk.ijse.liveChatRoom.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class LoginFormController {
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnExitOnAction(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    void signInBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signUpBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtUserNameOnAction(ActionEvent event) {
+
+    }
 }
